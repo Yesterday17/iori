82902098d71faec37c19c955c0f2c40f7468783b
Yesterday17 <mmf@mmf.moe>
2024-02-15 14:19:42 +0800

    Expose all fields in M3u8Segment

--- src/hls.rs
+++ src/hls.rs
@@ -11,18 +11,18 @@ pub use core::M3u8ListSource;
 pub use live::CommonM3u8LiveSource;
 
 pub struct M3u8Segment {
-    url: reqwest::Url,
-    filename: String,
+    pub url: reqwest::Url,
+    pub filename: String,
 
-    key: Option<Arc<decrypt::M3u8Key>>,
-    initial_segment: Option<Arc<Vec<u8>>>,
+    pub key: Option<Arc<decrypt::M3u8Key>>,
+    pub initial_segment: Option<Arc<Vec<u8>>>,
 
-    byte_range: Option<m3u8_rs::ByteRange>,
+    pub byte_range: Option<m3u8_rs::ByteRange>,
 
     /// Sequence id allocated by the downloader
-    sequence: u64,
+    pub sequence: u64,
     /// Media sequence id from the m3u8 file
-    media_sequence: u64,
+    pub media_sequence: u64,
 }
 
 impl crate::StreamingSegment for M3u8Segment {
