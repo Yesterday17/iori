f4cd850fbd4eeb2d8c7f7e776d5e90c9841f471d
Yesterday17 <mmf@mmf.moe>
2024-06-08 12:46:27 +0800

    feat: return `SegmentInfo` for `SequencialDownloader`

--- crates/iori/src/download/sequencial.rs
+++ crates/iori/src/download/sequencial.rs
@@ -15,15 +15,20 @@ where
         Self { source }
     }
 
-    pub async fn download(&mut self) -> IoriResult<()> {
+    pub async fn download(&mut self) -> IoriResult<Vec<S::SegmentInfo>> {
         let mut receiver = self.source.fetch_info().await?;
 
+        let mut segments_info = Vec::new();
         while let Some(segment) = receiver.recv().await {
             for segment in segment? {
+                if let Some(segment_info) = self.source.fetch_segment_info(&segment).await {
+                    segments_info.push(segment_info);
+                }
+
                 self.source.fetch_segment(&segment, false).await?;
             }
         }
 
-        Ok(())
+        Ok(segments_info)
     }
 }
