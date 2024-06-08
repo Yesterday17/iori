cd295ab9b607e0969e40521005feea331e0d9ce7
Yesterday17 <mmf@mmf.moe>
2024-06-08 13:24:23 +0800

    fix: `mkvmerge_merge` can merge video-only sources now

--- crates/iori/src/merge.rs
+++ crates/iori/src/merge.rs
@@ -84,60 +84,71 @@ where
     P: AsRef<Path>,
     O: AsRef<Path>,
 {
+    let mut tracks = Vec::new();
+
     // 1. merge videos with mkvmerge
     let mut videos: Vec<_> = segments
         .iter()
         .filter(|info| info.r#type() == SegmentType::Video)
         .collect();
-    videos.sort_by(|a, b| a.sequence().cmp(&b.sequence()));
-    let mut video = Command::new("mkvmerge")
-        .current_dir(&cwd)
-        .arg("-q")
-        .arg("[")
-        .args(videos.iter().map(|info| {
-            let filename = format!("{:06}_{}", info.sequence(), info.file_name());
-            filename
-        }))
-        .arg("]")
-        .arg("-o")
-        .arg("iori_video.mkv")
-        .spawn()?;
+    if !videos.is_empty() {
+        videos.sort_by(|a, b| a.sequence().cmp(&b.sequence()));
+        let video_path = cwd.as_ref().join("iori_video.mkv");
+
+        let mut video = Command::new("mkvmerge")
+            .current_dir(&cwd)
+            .arg("-q")
+            .arg("[")
+            .args(videos.iter().map(|info| {
+                let filename = format!("{:06}_{}", info.sequence(), info.file_name());
+                filename
+            }))
+            .arg("]")
+            .arg("-o")
+            .arg(&video_path)
+            .spawn()?;
+        video.wait().await?;
+        tracks.push(video_path);
+    }
 
     // 2. merge audios with mkvmerge
     let mut audios: Vec<_> = segments
         .iter()
         .filter(|info| info.r#type() == SegmentType::Audio)
         .collect();
-    audios.sort_by(|a, b| a.sequence().cmp(&b.sequence()));
-    let mut audio = Command::new("mkvmerge")
-        .current_dir(&cwd)
-        .arg("-q")
-        .arg("[")
-        .args(audios.iter().map(|info| {
-            let filename = format!("{:06}_{}", info.sequence(), info.file_name());
-            filename
-        }))
-        .arg("]")
-        .arg("-o")
-        .arg("iori_audio.mkv")
-        .spawn()?;
+    if !audios.is_empty() {
+        audios.sort_by(|a, b| a.sequence().cmp(&b.sequence()));
+        let audio_path = cwd.as_ref().join("iori_audio.mkv");
 
-    video.wait().await?;
-    audio.wait().await?;
+        let mut audio = Command::new("mkvmerge")
+            .current_dir(&cwd)
+            .arg("-q")
+            .arg("[")
+            .args(audios.iter().map(|info| {
+                let filename = format!("{:06}_{}", info.sequence(), info.file_name());
+                filename
+            }))
+            .arg("]")
+            .arg("-o")
+            .arg(&audio_path)
+            .spawn()?;
+        audio.wait().await?;
+        tracks.push(audio_path);
+    }
 
     // 3. merge audio and video
     let mut merge = Command::new("mkvmerge")
         .current_dir(&cwd)
-        .arg("iori_video.mkv")
-        .arg("iori_audio.mkv")
+        .args(tracks.iter())
         .arg("-o")
         .arg(output.as_ref())
         .spawn()?;
     merge.wait().await?;
 
     // 4. remove temporary files
-    tokio::fs::remove_file(cwd.as_ref().join("iori_video.mkv")).await?;
-    tokio::fs::remove_file(cwd.as_ref().join("iori_audio.mkv")).await?;
+    for track in tracks {
+        tokio::fs::remove_file(track).await?;
+    }
 
     Ok(())
 }
