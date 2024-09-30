cfff59f0bf92eac471d9bd91a5dc20be1e5af493
Yesterday17 <mmf@mmf.moe>
2024-09-30 11:44:32 +0900

    feat(nicolive): build using protobuf-src

--- crates/nicolive/build.rs
+++ crates/nicolive/build.rs
@@ -1,5 +1,7 @@
 use std::io::Result;
 fn main() -> Result<()> {
+    std::env::set_var("PROTOC", protobuf_src::protoc());
+
     prost_build::compile_protos(
         &[
             "src/proto/dwango/nicolive/chat/data/atoms.proto",
