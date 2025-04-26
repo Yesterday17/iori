7da6de9d3507ba0846673b50a1deb233ed8dbef5
Yesterday17 <mmf@mmf.moe>
2025-04-26 14:13:48 +0900

    fix(shiori): pipe

--- bin/shiori/src/commands/download.rs
+++ bin/shiori/src/commands/download.rs
@@ -283,6 +283,12 @@ impl OutputOptions {
     pub fn into_merger(self) -> IoriMerger {
         if self.no_merge {
             IoriMerger::skip()
+        } else if self.pipe {
+            IoriMerger::pipe(true)
+        } else if self.pipe_mux {
+            IoriMerger::pipe_mux(true, "-".into(), None)
+        } else if let Some(file) = self.pipe_to {
+            IoriMerger::pipe_to_file(true, file)
         } else if let Some(mut output) = self.output {
             if output.exists() {
                 log::warn!("Output file exists. Will add suffix automatically.");
@@ -299,12 +305,6 @@ impl OutputOptions {
             } else {
                 IoriMerger::auto(output, false)
             }
-        } else if self.pipe {
-            IoriMerger::pipe(true)
-        } else if self.pipe_mux {
-            IoriMerger::pipe_mux(true, "-".into(), None)
-        } else if let Some(file) = self.pipe_to {
-            IoriMerger::pipe_to_file(true, file)
         } else {
             unreachable!()
         }
