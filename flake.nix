918aa245e93ac463dcc7e0cbdf03c8808a138752
Yesterday17 <mmf@mmf.moe>
2025-04-28 22:17:37 +0800

    feat(iori): introduce `flake.nix`

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "Iori";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      naersk,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        naersk-lib = pkgs.callPackage naersk { };
+
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [
+            "rust-src"
+            "rust-analyzer"
+          ];
+        };
+      in
+      {
+        packages.default = naersk-lib.buildPackage {
+          src = ./.;
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+          buildInputs = with pkgs; [
+            protobuf
+          ];
+          cargoBuildOptions = opts: opts ++ [ "--workspace" ];
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustToolchain
+            pkg-config
+            rust-analyzer
+            protobuf
+          ];
+        };
+      }
+    );
+}
