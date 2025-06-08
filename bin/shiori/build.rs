4ac27e26cfcaeaf622da0e8c553a612468497fa9
Yesterday17 <mmf@mmf.moe>
2025-06-08 22:03:47 +0800

    refactor(shiori): use `winresource` to  set manifest

--- bin/shiori/build.rs
+++ bin/shiori/build.rs
@@ -15,30 +15,12 @@ fn main() {
     let hash = get_commit_hash().unwrap_or_else(|_| "unknown".to_string());
     println!("cargo:rustc-env=SHIORI_VERSION={version} ({hash})");
 
-    let target_os = env::var("CARGO_CFG_TARGET_OS");
-    let target_env = env::var("CARGO_CFG_TARGET_ENV");
-    if Ok("windows") == target_os.as_deref() && Ok("msvc") == target_env.as_deref() {
-        set_windows_exe_options();
+    if let Ok("windows") = std::env::var("CARGO_CFG_TARGET_OS").as_deref() {
+        let mut res = winresource::WindowsResource::new();
+        res.set_manifest_file("windows.manifest.xml");
+        res.compile().unwrap();
     } else {
         // Avoid rerunning the build script every time.
         println!("cargo:rerun-if-changed=build.rs");
     }
-}
-
-// Add a manifest file to rustc.exe.
-fn set_windows_exe_options() {
-    static WINDOWS_MANIFEST_FILE: &str = "windows.manifest.xml";
-
-    let mut manifest = env::current_dir().unwrap();
-    manifest.push(WINDOWS_MANIFEST_FILE);
-
-    println!("cargo:rerun-if-changed={WINDOWS_MANIFEST_FILE}");
-    // Embed the Windows application manifest file.
-    println!("cargo:rustc-link-arg-bin=shiori=/MANIFEST:EMBED");
-    println!(
-        "cargo:rustc-link-arg-bin=shiori=/MANIFESTINPUT:{}",
-        manifest.to_str().unwrap()
-    );
-    // Turn linker warnings into errors.
-    println!("cargo:rustc-link-arg-bin=shiori=/WX");
 }
